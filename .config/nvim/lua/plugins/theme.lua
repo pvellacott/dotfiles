3ee7f9ebe3ea1086dcc8c0c2c4d99205606b6b8b
Phil Vellacott <philvellacott@proton.me>
2025-08-28 12:03:26 +1200

    omarchy yoink

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,4 +1,5 @@
 return {
-    { "vague2k/vague.nvim", lazy = false, priority = 1000 },
-    { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+	{ "vague2k/vague.nvim", lazy = false, priority = 1000 },
+	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+	{ "ribru17/bamboo.nvim", lazy = false, priority = 1000 },
 }
